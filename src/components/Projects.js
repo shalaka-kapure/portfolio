29017da929ee70b6c18de462f83f6106243a8d89
Shalaka <shalakakapure@gmail.com>
2025-02-04 15:31:10 +0530

    added new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React from "react";
 import bazaar from "../assest/projects/bazaar.png";
 import redux from "../assest/projects/redux.png";
+import dashboard from "../assest/projects/dashboard.png";
+import taskify from "../assest/projects/taskify.png";
 import { Col, Container, Row, Tab } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import colorSharp2 from "../assest/img/color-sharp2.png";
@@ -15,6 +17,18 @@ const Projects = () => {
       description: "Ecommerce website",
       imgUrl: bazaar,
       href: "https://bazaar-com.netlify.app/",
+    },
+    {
+      title: "Taskify - your Go-to To-do List app",
+      description: "Add, Edit, Delete tasks to your to-do list",
+      imgUrl: taskify,
+      href: "https://taskify-shalaka.netlify.app/",
+    },
+    {
+      title: "Dashboard",
+      description: "Recreated a Dashboard UI",
+      imgUrl: dashboard,
+      href: "https://dashboard-shalaka.netlify.app/",
     },
     {
       title: "Simple React Redux website",
@@ -60,13 +74,38 @@ const Projects = () => {
                     >
                       <Tab.Pane eventKey="first">
                         <Row>
-                          {project.map((project, index) => {
-                            return <ProjectCard key={index} {...project} />;
-                          })}
+                          {project.length > 0 &&
+                            project
+                              .slice(0, 3)
+                              .map((project, index) => (
+                                <ProjectCard key={index} {...project} />
+                              ))}
                         </Row>
                       </Tab.Pane>
-                      <Tab.Pane eventKey="second"></Tab.Pane>
-                      <Tab.Pane eventKey="third"></Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {project.length > 3 &&
+                            project
+                              .slice(3, 6)
+                              .map((project, index) => (
+                                <ProjectCard key={index} {...project} />
+                              ))}
+                        </Row>
+                        {project.length <= 3 && <p>No more projects yet</p>}
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <Row>
+                          {project.length > 6 &&
+                            project
+                              .slice(6, 9)
+                              .map((project, index) => (
+                                <ProjectCard key={index} {...project} />
+                              ))}
+                        </Row>
+                        {project.length <= 6 && (
+                          <p>We are working on new projects</p>
+                        )}
+                      </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
                 </div>
